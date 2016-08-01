09679533c8a258a950d22b01355ee8bef4348d24
WJohnson1 <williamisamazing1@gmail.com>
2016-08-01 15:14:19 -0400

    Update and rename snake.js to Snake.js

---
+++ Snake.js
@@ -0,0 +1,8 @@
+function setup(){
+  createCanvas(500,500);
+  background(0);
+}
+function draw(){
+  fill(255,0,0);
+  rect(25,25,25,25);
+}
--- snake.js
+++
@@ -1,8 +0,0 @@
-function setup(){
-createCanvas(500,500);
-background(0);
-}
-function draw(){
-fill(255,0,0);
-rect(25,25,25,25);
-}
